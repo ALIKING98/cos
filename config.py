927a1c130d2382d9d5f8b0b50a5fe5e168eea00d
ALIKING98 <59519533+ALIKING98@users.noreply.github.com>
2022-11-15 19:00:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("SESSION_NAME", "AQBD1bz4x0zqW4D7FtgA_v-f5BZwvzCebbE1k4j_nF7ewZazQSlzkdvz2HPhqEkzoftNAmz1Cx1P5SJZun2vawxdQlOV3ohcx7hecJvIr8whyz3P5n5X4iWlkpFADI-Md3Jq8N1mHHHeO4bmMF3C6MK-2zNqp-5nquMihRLqZ0Cb2R2UK4QgKa-h9TuMFy2sTP2HQv3reBGIkKYoavhyFNTYVmg3Q2VwyaPCenyLXpHdLhmADgcY-aH7BQ5HF8cuk-vqu_7PpPJZNl7AEzLIIIlvddtXVwHM1ST0CJAIrB_Gjy_W9E5riCCMrbnIJnvEYo5GPuaDVKly_i7Z_UHKe0vvAAAAAVas6LwA")
 BOT_TOKEN = getenv("BOT_TOKEN", "5634980934:AAErWBn1rmbh_klCYM5jsDHIZI-PjELVMCQ")
 BOT_NAME = getenv("BOT_NAME", "NOT MUSIC")
 API_ID = int(getenv("API_ID", "20763469")
